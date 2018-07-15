167265c043cfbfbf88992a0f4fb4cd2e6fc00b06
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-15 19:50:45 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,30 @@
 # Code your instances here
+class GardenGnome
+attr_accessor :name, :age, :gluten_allergy
+attr_reader :personality, :hat_color
+
+def initialize(hat_color = "red")
+  @personality = "evil"
+  @hat_color = hat_color
+
+end
+def gnaw
+return "Gnawing on a tree!!!"
+end   
+def shout
+return "GNARLY!!!"
+end 
+def introduce_self
+  return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+end 
+  
+end 
+test_gnome = GardenGnome.new
+test_gnome.name = "Felicia"
+test_gnome.age = 5
+test_gnome.gluten_allergy = "True"
+puts test_gnome.personality
+puts test_gnome.name
+puts test_gnome.age 
+puts test_gnome.gluten_allergy
+
